c68b4ea49b6f86020693f9dc098dff0c85f7b105
Rafał Giemza <rafal.giemza@intive.com>
2019-06-06 12:23:01 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+(function(){
+    const body = document.querySelector('div');
+    if (body) {
+        const randomPass = generateRandomPassword(20);
+        body.innerHTML = randomPass;
+    }
+
+    function generateRandomPassword(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+}())
